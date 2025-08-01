099b3b322d8595afb84f112126b1892869611a27
Dina Adel <dinaadelshehat43@gmail.com>
2025-08-01 17:06:01 +0200

    add futuer list view to home

--- lib/features/home/presentation/views/widegts/home_view_body.dart
+++ lib/features/home/presentation/views/widegts/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
+import 'featured_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -11,7 +11,7 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        CustomListViewItem(),
+        FeaturedBooksListView(),
       ],
     );
   }
