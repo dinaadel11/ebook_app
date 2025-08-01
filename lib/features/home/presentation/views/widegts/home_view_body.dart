9e9ea2d78425b16c222b09c7363c89c706f52cbf
Dina Adel <dinaadelshehat43@gmail.com>
2025-08-01 17:27:08 +0200

    create styles file and add titel home view

--- lib/features/home/presentation/views/widegts/home_view_body.dart
+++ lib/features/home/presentation/views/widegts/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/core/utils/styles.dart';
 
 import 'custom_app_bar.dart';
 import 'featured_books_list_view.dart';
@@ -8,11 +9,19 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          Text(
+            'Best Seller',
+            style: Styles.titelMedium,
+          )
+        ],
+      ),
     );
   }
 }
