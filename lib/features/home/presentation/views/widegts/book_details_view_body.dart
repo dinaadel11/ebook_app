8c8d210c839ee5b32c5ded2182a8a6caffdb2193
Dina Adel <dinaadelshehat43@gmail.com>
2025-08-02 23:18:11 +0200

    add titel and subtitel and rating

--- lib/features/home/presentation/views/widegts/book_details_view_body.dart
+++ lib/features/home/presentation/views/widegts/book_details_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:newsapp/features/home/presentation/views/widegts/book_rating.dart';
 import 'custom_book_details_app_bar.dart';
 import 'custom_book_item.dart';
 
@@ -14,8 +16,34 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          Text(
+            'The Jungle Book',
+            style: GoogleFonts.ebGaramond(
+                fontSize: 30, fontWeight: FontWeight.normal),
+            maxLines: 2,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Text(
+            'Rudyard Kipling',
+            style: GoogleFonts.ebGaramond(
+                fontSize: 20,
+                fontWeight: FontWeight.normal,
+                color: const Color.fromARGB(255, 182, 181, 181)),
+            maxLines: 2,
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ],
       ),
--- lib/features/home/presentation/views/widegts/book_rating.dart
+++ lib/features/home/presentation/views/widegts/book_rating.dart
@@ -3,21 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:newsapp/core/utils/styles.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           Icons.star,
           color: Color(0xffFFDD4F),
         ),
         const SizedBox(
-          width: 6.3,
+          width: 10,
         ),
         const Text(
-          '4.8',
+          '6',
           style: Styles.textstyle16,
         ),
         const SizedBox(
